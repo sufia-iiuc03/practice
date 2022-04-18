3c40fba229b24ca7b5fc2780fae1e5cee70f5925
sufia-iiuc03 <sufia.iiuc03@gmail.com>
2022-04-18 20:05:06 +0600

    some modification at Sum file

--- sum.java
+++ sum.java
@@ -1,6 +1,7 @@
 public class Sum{
     public static void main(String[] args){
-        int a = 20;
-        System.out.println(a);
+       public int a = 20;
+       public int b = 50;
+        System.out.println(a+b);
     }
 }
