a7d7711c8c30b88249598e3b94b9168fa4cc4f39
sufia-iiuc03 <sufia.iiuc03@gmail.com>
2022-04-24 12:58:28 +0600

    remove all method

--- sum.java
+++ sum.java
@@ -1,19 +1,10 @@
 public class Sum{
     public static void main(String[] args){
-         int a = 20;
-         int b = 50;
-        System.out.println("Result of Summation : ");
-        System.out.println(a+b);
-        multi(a,b);
-        division(a,b);
+      
+        System.out.println(" this is from branch 3 ");
+        
+      
     }
-    public static void  multi(int a,int b){
-         System.out.println("Result of Multiplication : ");
-        System.out.println(a*b);
-    }
-      public static void  division(int a,int b){
-         System.out.println("Result of division : ");
-        System.out.println(a*b);
-    }
+   
     
 }
