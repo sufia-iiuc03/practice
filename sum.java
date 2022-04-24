f7e127a8e066c1cb50b74e09548f318e8be44e50
sufia-iiuc03 <sufia.iiuc03@gmail.com>
2022-04-24 13:01:44 +0600

    update branch 3

--- sum.java
+++ sum.java
@@ -1,10 +1,12 @@
 public class Sum{
     public static void main(String[] args){
-      
-        System.out.println(" this is from branch 3 ");
-        
-      
+         int a = 20;
+         int b = 50;
+        System.out.println("Result of Summation : ");
+        System.out.println(a+b);
+        branch3();
     }
-   
-    
+    public static void  branch3( ){
+        System.out.println(" this is from branch 3 ");
+    }
 }
