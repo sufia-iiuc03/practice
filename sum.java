cdc37965068eddbfe5f1cecbdcc84a2e74d5132b
sufia-iiuc03 <sufia.iiuc03@gmail.com>
2022-04-24 11:58:47 +0600

    changes at testBranch

--- sum.java
+++ sum.java
@@ -2,6 +2,7 @@ public class Sum{
     public static void main(String[] args){
          int a = 20;
          int b = 50;
+        System.out.println("Result of Summation : ");
         System.out.println(a+b);
         multi(a,b);
     }
